863768dff9a571e553b6f3d245d8ab60bf921f04
Nachalnik26 <72456569+Nachalnik26@users.noreply.github.com>
2020-12-12 17:40:54 +0300

    Add files via upload

---
+++ TestWebApp_1/IVideosCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebApp_1.Data.Models;
+
+namespace TestWebApp_1.Data.Interfaces
+{
+    interface IVideosCategory
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
